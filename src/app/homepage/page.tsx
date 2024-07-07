4b7d52c4cc3b82c1d3dafb135f493b03b41772dd
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-07-08 00:51:18 +0200

    add homepage mobile

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./homepage.module.css";
 import Link from "next/link";
 import { Button } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 
 export default function Homepage() {
+  const isMobile = useMediaQuery("(max-width: 850px)");
+
+  console.log("isMobile", isMobile);
   return (
     <div className={styles.containerWidth}>
       <div className={styles.imageContainer}>
@@ -13,7 +19,8 @@ export default function Homepage() {
             variant="filled"
             color="rgba(249, 100, 1, 1)"
             radius="xl"
-            size="xl"
+            size={isMobile ? "xs" : "xl"}
+            // size="xl"
             classNames={{
               label: styles.buttonLabel,
             }}
