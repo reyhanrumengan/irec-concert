27d6a4132f21857858d568fb7b5f8913de94b6ff
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-07-08 00:11:10 +0200

    finish program

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -12,8 +12,8 @@ export default function Program() {
 
           <div>
             <p className={styles.pieceTitle}>3 Romances, Op. 22</p>
-            <p>Clara Schumann (1853)</p>
-            <p>
+            <p className={styles.composer}>Clara Schumann (1853)</p>
+            <p className={styles.description}>
               A set of three lyrical and expressive pieces that showcase Clara
               Schumann&rsquo;s emotional depth and technical skill, blending
               Romantic sensibilities with her unique musical voice.
@@ -24,8 +24,8 @@ export default function Program() {
             <p className={styles.pieceTitle}>
               Pièces de Clavecin en Concert No. 5
             </p>
-            <p>Jean-Philippe Rameau (1741)</p>
-            <p>
+            <p className={styles.composer}>Jean-Philippe Rameau (1741)</p>
+            <p className={styles.description}>
               A vibrant and intricate work that reflects Rameau&rsquo;s
               innovative harmonic language and the elegance of the French
               Baroque style, designed for harpsichord accompanied by violin and
@@ -35,8 +35,10 @@ export default function Program() {
 
           <div>
             <p className={styles.pieceTitle}>Suite in E Minor</p>
-            <p>Jean-Philippe Rameau (Early 18th Century)</p>
-            <p>
+            <p className={styles.composer}>
+              Jean-Philippe Rameau (Early 18th Century)
+            </p>
+            <p className={styles.description}>
               A collection of ornate and expressive harpsichord pieces,
               highlighting Rameau&rsquo;s mastery of keyboard composition and
               the intricate ornamentation typical of the Baroque period.
@@ -47,8 +49,8 @@ export default function Program() {
             <p className={styles.pieceTitle}>
               Membra Jesu Nostri, BuxWV 75 (Part 1 & 2)
             </p>
-            <p>Dieterich Buxtehude (1680)</p>
-            <p>
+            <p className={styles.composer}>Dieterich Buxtehude (1680)</p>
+            <p className={styles.description}>
               The first two parts of a cycle of seven cantatas, focusing on the
               feet and knees of Christ. These sections feature profound text
               settings and rich Baroque musical textures, reflecting deep
