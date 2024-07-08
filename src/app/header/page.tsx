b8c7eb4e306c0a4accd07d201bc7cd90ccc05ff3
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-07-08 13:10:45 +0200

    add link and comment

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -7,14 +7,16 @@ export default function Header() {
   return (
     <div className={styles.containerWidth}>
       <div className={styles.navigation}>
-        <Image
-          width={1080}
-          height={720}
-          src="/logo.svg"
-          alt="Logo"
-          className={styles.logo}
-          layout="responsive"
-        />
+        <Link href="/">
+          <Image
+            width={1080}
+            height={720}
+            src="/logo.svg"
+            alt="Logo"
+            className={styles.logo}
+            layout="responsive"
+          />
+        </Link>
         <div style={{ flex: "1 1 0px" }}></div>
 
         <Link className={styles.navigationItem} href="/">
