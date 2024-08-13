c4f7547830522d7b6eda68d274a8a33772fd2de6
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-08-13 08:24:01 +0200

    add hannah's profile

--- src/app/musicians/page.tsx
+++ src/app/musicians/page.tsx
@@ -193,6 +193,30 @@ export default function Musicians() {
               }}
             />
           </div>
+
+          <p className={styles.artistName}>Hannah Jonina Ivy</p>
+          <div className={styles.artistDescription}>
+            Hannah Jonina Ivy was born in Indonesia, began her violin studies at
+            the age of twelve. She is currently pursuing her musical degree at
+            the Hochschule für Musik, Theater und Medien Hannover. Hannah had
+            participated in numerous masterclasses and received private lessons
+            from various teachers. She is not only an active performer,
+            frequently playing in orchestras and ensembles, but also passionate
+            about teaching. Hannah is also active in IREC Europe, where she
+            mainly contributes as a choir member and musician.
+            <Image
+              width={1080}
+              height={720}
+              className={styles.artistImage}
+              src="/artist-hannah3.png"
+              alt="Hannah's Photo"
+              sizes="100vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
