c3780bb638908197d373b71864a15faee20a43ce
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2024-08-30 22:19:06 +0200

    add piece

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -8,12 +8,37 @@ export default function Program() {
           <p className={styles.pageTitle}>Concert Program</p>
 
           <div>
-            <p className={styles.pieceTitle}>3 Romances, Op. 22</p>
+            <p className={styles.pieceTitle}>
+              3 Romances for Violin and Piano, Op. 22
+            </p>
             <p className={styles.composer}>Clara Schumann (1853)</p>
             <p className={styles.description}>
               A set of three lyrical and expressive pieces that showcase Clara
               Schumann&rsquo;s emotional depth and technical skill, blending
               Romantic sensibilities with her unique musical voice.
+            </p>
+          </div>
+
+          <div>
+            <p className={styles.pieceTitle}>Allegro in B minor Op. 8</p>
+            <p className={styles.composer}>Robert Schumann (1831)</p>
+            <p className={styles.description}>
+              Robert Schumann's Allegro in B minor, Op. 8 is stands out as one
+              of Schumann's earlier compositions, showcasing his burgeoning
+              Romantic style, which is characterized by emotional depth, complex
+              harmonies, and an exploration of dramatic contrasts.
+            </p>
+          </div>
+
+          <div>
+            <p className={styles.pieceTitle}>Suite in E Minor</p>
+            <p className={styles.composer}>
+              Jean-Philippe Rameau (Early 18th Century)
+            </p>
+            <p className={styles.description}>
+              A collection of ornate and expressive harpsichord pieces,
+              highlighting Rameau&rsquo;s mastery of keyboard composition and
+              the intricate ornamentation typical of the Baroque period.
             </p>
           </div>
 
@@ -27,18 +52,6 @@ export default function Program() {
               innovative harmonic language and the elegance of the French
               Baroque style, designed for harpsichord accompanied by violin and
               cello.
-            </p>
-          </div>
-
-          <div>
-            <p className={styles.pieceTitle}>Suite in E Minor</p>
-            <p className={styles.composer}>
-              Jean-Philippe Rameau (Early 18th Century)
-            </p>
-            <p className={styles.description}>
-              A collection of ornate and expressive harpsichord pieces,
-              highlighting Rameau&rsquo;s mastery of keyboard composition and
-              the intricate ornamentation typical of the Baroque period.
             </p>
           </div>
 
