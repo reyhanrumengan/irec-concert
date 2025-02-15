e74aa377791ab1f2c2cf66b8fbd3e14b02465468
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2025-02-15 12:39:12 +0100

    finish homepage

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -33,7 +33,7 @@ export default function Homepage() {
           <Image
             width={760}
             height={760}
-            src="/logo2.svg"
+            src="/poster-hamburg.svg"
             alt="Concert Poster"
             className={styles.poster}
             priority
