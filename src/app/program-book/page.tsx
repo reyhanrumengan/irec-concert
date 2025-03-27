9bc72c6e74038d56d1b5a09b9bb3c7fd88966026
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2025-03-27 18:41:14 +0100

    add programbook

--- src/app/program-book/page.tsx
+++ src/app/program-book/page.tsx
@@ -1,13 +1,38 @@
+// "use client";
 import styles from "./program-book.module.css";
+import Image from "next/image";
 
 export default function ProgramBook() {
-  const pdfFrame = () => {
-    return (
-      <div style={{ width: "100%", height: "100vh" }}>
-        <iframe src="/program-book.pdf" width="100%" height="100%" />
-      </div>
-    );
+  const pdfImages = () => {
+    return <div style={{ width: "100%", height: "100vh" }}></div>;
   };
+
+  const programBookImages = [];
+
+  for (let i = 1; i <= 12; i++) {
+    programBookImages.push({
+      src: `/program-book/programBook${i}.png`,
+      alt: `Program Book - Page ${i}`,
+    });
+  }
+
+  const programBook = programBookImages.map((image, index) => (
+    <div key={index} className={styles.pageContainer}>
+      <Image
+        width={760}
+        height={760}
+        className={styles.programBookImage}
+        alt={image.alt}
+        src={image.src}
+        priority
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
+    </div>
+  ));
 
   return (
     <div className={styles.containerWidth}>
@@ -15,7 +40,7 @@ export default function ProgramBook() {
         <div className={styles.program}>
           <p className={styles.pageTitle}>Program Book</p>
 
-          {pdfFrame()}
+          {programBook}
         </div>
       </div>
     </div>
