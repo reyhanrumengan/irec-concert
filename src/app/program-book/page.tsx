2a05c59d82ebfa95af472880ecb3a38da28b559c
Reyhan Rumengan <reyhanrumengan98@gmail.com>
2025-10-18 00:35:28 +0200

    add program book v2

--- src/app/program-book/page.tsx
+++ src/app/program-book/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function ProgramBook() {
   const programBookImages = [];
 
-  for (let i = 1; i <= 8; i++) {
+  for (let i = 1; i <= 13; i++) {
     programBookImages.push({
       src: `/program-book/programBook${i}.png`,
       alt: `Program Book - Page ${i}`,
